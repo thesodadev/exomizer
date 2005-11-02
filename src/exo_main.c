2d97a0f7da46723e771d3b5e415cc49c965183a1
Magnus Lind <magli143@comhem.se>
2005-11-02 23:09:32 +0200

    Make exoraw into a exomizer sub command.
    + Introduce a new sub command 'raw'
    + Add documentation to exo20info.txt

--- src/exo_main.c
+++ src/exo_main.c
@@ -42,7 +42,7 @@
 
 extern struct membuf sfxdecr[];
 
-#define DEFAULT_OUTFILE "a.prg"
+#define DEFAULT_OUTFILE "a.out"
 
 static
 FILE *
@@ -414,7 +414,7 @@ void print_usage(const char *appl, enum log_level level)
 {
     /* done */
     LOG(level,
-        ("usage: %s level|mem|sfx [option]... infile[,<address>]...\n"
+        ("usage: %s level|mem|sfx|raw [option]... infile[,<address>]...\n"
          "  see the individual commands for more help.\n",
          appl));
 }
@@ -450,6 +450,18 @@ void print_mem_usage(const char *appl, enum log_level level,
     LOG(level,
         (" All infiles are merged into the outfile. They are loaded in the order\n"
          " they are given on the command-line, from left to right.\n"));
+}
+
+static
+void print_raw_usage(const char *appl, enum log_level level,
+                     const char *default_out_name)
+{
+    LOG(level, ("usage: %s [option]... infile\n", appl));
+    LOG(level,
+        ("  -b            crunch/decrunch backwards\n"
+         "  -r            write outfile in reverse order\n"
+         "  -d            decrunch (instead of crunch)\n"));
+    print_shared_flags(level, default_out_name);
 }
 
 static
@@ -798,7 +810,7 @@ void sfx(const char *appl, int argc, char *argv[])
         {
             /* we got an address we should jmp to. */
             LOG(LOG_ERROR,
-                ("Error: invalid address for -s option, "
+                ("Error: invalid address for <start>, "
                  "must be in the range of [0 - 0xffff]\n"));
             print_sfx_usage(appl, LOG_NORMAL, DEFAULT_OUTFILE);
             exit(-1);
@@ -1055,6 +1067,128 @@ void sfx(const char *appl, int argc, char *argv[])
     parse_free();
 }
 
+void raw(const char *appl, int argc, char *argv[])
+{
+    char flags_arr[32];
+    int decrunch_mode = 0;
+    int backwards_mode = 0;
+    int reverse_mode = 0;
+    int c, infilec;
+    char **infilev;
+
+    static struct crunch_options options[1] = { CRUNCH_OPTIONS_DEFAULT };
+    struct common_flags flags[1] = { {options, DEFAULT_OUTFILE} };
+
+    struct membuf inbuf[1];
+    struct membuf outbuf[1];
+
+    LOG(LOG_DUMP, ("flagind %d\n", flagind));
+    sprintf(flags_arr, "bdr%s", SHARED_FLAGS);
+    while ((c = getflag(argc, argv, flags_arr)) != -1)
+    {
+        LOG(LOG_DUMP, (" flagind %d flagopt '%c'\n", flagind, c));
+        switch (c)
+        {
+        case 'b':
+            backwards_mode = 1;
+            break;
+        case 'r':
+            reverse_mode = 1;
+            break;
+        case 'd':
+            decrunch_mode = 1;
+            break;
+        default:
+            handle_shared_flags(c, flagarg, print_raw_usage, appl, flags);
+        }
+    }
+
+    infilev = argv + flagind;
+    infilec = argc - flagind;
+
+    if (infilec != 1)
+    {
+        LOG(LOG_ERROR, ("Error: exactly one input file must be given.\n"));
+        print_raw_usage(appl, LOG_NORMAL, DEFAULT_OUTFILE);
+        exit(-1);
+    }
+
+    membuf_init(inbuf);
+    membuf_init(outbuf);
+
+    read_file(infilev[0], inbuf);
+
+    if(decrunch_mode)
+    {
+        int seems_backward = 0;
+        int seems_forward = 0;
+        unsigned char *p;
+
+        p = membuf_get(inbuf);
+        if(p[0] == 0x80 && p[1] == 0x0)
+        {
+            seems_backward = 1;
+        }
+        p += membuf_memlen(inbuf);
+        if(p[-1] == 0x80 && p[-2] == 0x0)
+        {
+            seems_forward = 1;
+        }
+
+        /* do we know what way it was crunched? */
+        if((seems_backward ^ seems_forward) != 0)
+        {
+            /* yes, override option. */
+            backwards_mode = seems_backward;
+        }
+
+        if(backwards_mode)
+        {
+            LOG(LOG_NORMAL, ("Decrunching infile \"%s\" to outfile \"%s\" "
+                             " backwards.\n", infilev[0], flags->outfile));
+            decrunch_backwards(LOG_NORMAL, inbuf, outbuf);
+        }
+        else
+        {
+            LOG(LOG_NORMAL, ("Decrunching infile \"%s\" to outfile \"%s\".\n",
+                             infilev[0], flags->outfile));
+            decrunch(LOG_NORMAL, inbuf, outbuf);
+        }
+    }
+    else
+    {
+        struct crunch_info info[1];
+        if(backwards_mode)
+        {
+            LOG(LOG_NORMAL, ("Crunching infile \"%s\" to outfile \"%s\" "
+                             "backwards.\n", infilev[0], flags->outfile));
+            crunch_backwards(inbuf, outbuf, options, info);
+        }
+        else
+        {
+            LOG(LOG_NORMAL, ("Crunching infile \"%s\" to outfile \"%s\".\n",
+                             infilev[0], flags->outfile));
+            crunch(inbuf, outbuf, options, info);
+        }
+
+        LOG(LOG_NORMAL, (" Literal sequences are %sused and",
+                         info->literal_sequences_used ? "" : "not "));
+        LOG(LOG_NORMAL, (" the safety offset is %d.\n",
+                         info->needed_safety_offset));
+
+    }
+
+    if(reverse_mode)
+    {
+        reverse_buffer(membuf_get(outbuf), membuf_memlen(outbuf));
+    }
+
+    write_file(flags->outfile, outbuf);
+
+    membuf_free(outbuf);
+    membuf_free(inbuf);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -1086,6 +1220,10 @@ main(int argc, char *argv[])
     else if(strcmp(argv[0], "sfx") == 0)
     {
         sfx(appl, argc, argv);
+    }
+    else if(strcmp(argv[0], "raw") == 0)
+    {
+        raw(appl, argc, argv);
     }
     else
     {
