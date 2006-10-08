4250510e445a5f28ce0ab0d1c6c66f5a0785622e
Magnus Lind <magli143@gmail.com>
2006-10-08 19:36:30 +0200

    Changed the version string too

--- src/exo_helper.c
+++ src/exo_helper.c
@@ -408,7 +408,7 @@ void print_license(void)
 {
     LOG(LOG_BRIEF,
         ("----------------------------------------------------------------------------\n"
-         "Exomizer v2.0beta4, Copyright (c) 2002 - 2006 Magnus Lind. (magli143@gmail.com)\n"
+         "Exomizer v2.0beta5, Copyright (c) 2002 - 2006 Magnus Lind. (magli143@gmail.com)\n"
          "----------------------------------------------------------------------------\n"));
     LOG(LOG_BRIEF,
         ("This software is provided 'as-is', without any express or implied warranty.\n"
